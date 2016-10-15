605086b6b73a02191a67eac0ce2877df8a5d8b00
Michal Novák <michal.novak@weborama.cz>
2016-10-15 21:17:39 +0200

    task category can be assigned on task creation

--- app/components/form/InsertTask.php
+++ app/components/form/InsertTask.php
@@ -6,15 +6,19 @@ use App\Model\Entity\Task;
 use App\Model\Entity\TaskGroup;
 use App\Model\Repository\TaskRepository;
 use App\Model\Repository\TaskGroupRepository;
+use App\Model\Repository\TaskCategoryRepository;
 use Nette\Application\UI\Control;
 use Nette\Application\UI\Form;
 
 class InsertTask extends Control
 {
+    
     /** @var TaskRepository */
     public $taskRepository;
     /** @var TaskGroupRepository */
     public $taskGroupRepository;
+    /** @var TaskCategoryRepository */
+    public $taskCategoryRepository;
     /** @var number */
     public $idTaskGroup;
     /** @var callable[] function(Control $sender, Task $task) */
@@ -23,12 +27,14 @@ class InsertTask extends Control
     /**
      * @param TaskRepository $taskRepository
      * @param TaskGroupRepository $taskGroupRepository
+     * @param TaskCategoryRepository $taskCategoryRepository
      */
-    public function __construct(TaskRepository $taskRepository, TaskGroupRepository $taskGroupRepository)
+    public function __construct(TaskRepository $taskRepository, TaskGroupRepository $taskGroupRepository, TaskCategoryRepository $taskCategoryRepository)
     {
         parent::__construct();
         $this->taskRepository = $taskRepository;
         $this->taskGroupRepository = $taskGroupRepository;
+        $this->taskCategoryRepository = $taskCategoryRepository;
     }
 
     public function render()
@@ -54,8 +60,17 @@ class InsertTask extends Control
         $form = new Form();
         $form->addText('name', 'Name')
             ->setRequired('Please fill task name');
+        
+        $categories = array(null => "");
+        foreach ($this->taskCategoryRepository->getAll() as $category) {
+            $categories[$category->getId()] = $category->getName();
+        }
+        
+        $form->addSelect('category', 'Category', $categories);
+        
         $form->addText('date', 'Date')
             ->setRequired('Please fill task date');
+        
         $form->addSubmit('submit', 'Add');
         $form->onSuccess[] = function($f, $v) {
             $this->onSuccess($f, $v);
@@ -72,17 +87,26 @@ class InsertTask extends Control
      */
     public function onSuccess(Form $form, $values)
     {
-        $taskGroup = $this->taskGroupRepository->getById($this->idTaskGroup);
-
         $taskEntity = new Task();
+        
         $taskEntity->setName($values->name);
         $taskEntity->setDate($values->date);
+        
+        if ($values->category) {
+            $taskCategory = $this->taskCategoryRepository->getById($values->category);
+            $taskEntity->setTaskCategory($taskCategory);
+        }
+        
+        $taskGroup = $this->taskGroupRepository->getById($this->idTaskGroup);
         $taskEntity->setTaskGroup($taskGroup);
+        
+        // persist
         $this->taskRepository->insert($taskEntity);
 
         // clear the form
         $form->setValues(array(), true);
 
+        // raise event
         $this->onTaskAdded($this, $taskEntity);
     }
 
