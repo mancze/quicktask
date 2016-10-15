b187cd6271f8cfc75ef92239733f7082e645be57
Michal Novák <michal.novak@weborama.cz>
2016-10-15 16:28:39 +0200

    added BasePresenter->redirectIfNotAjax

--- app/presenter/BasePresenter.php
+++ app/presenter/BasePresenter.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Presenters;
 
 use Nette;
@@ -8,4 +9,21 @@ use Nette;
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
+
+    /**
+     * Redirect to another presenter, action or signal unless the request is ajax.
+     * @param  int      [optional] HTTP error code
+     * @param  string   destination in format "[//] [[[module:]presenter:]action | signal! | this] [#fragment]"
+     * @param  array|mixed
+     * @return void
+     * @throws Nette\Application\AbortException
+     */
+    public function redirectIfNotAjax($code, $destination = NULL, $args = array())
+    {
+        if ($this->isAjax()) {
+            return;
+        }
+
+        return call_user_func_array(array($this, "redirect"), func_get_args());
+    }
 }
